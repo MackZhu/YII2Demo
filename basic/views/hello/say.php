687ef8ea910f39a20fb9b1b23aa9a4d952389833
MackZhu <mack.zhu@gmail.com>
2017-11-12 21:14:05 +0800

    T

--- basic/views/hello/say.php
+++ basic/views/hello/say.php
@@ -1,4 +1,18 @@
 <?php
 use yii\helpers\Html;
+use app\models\EntryForm;
+
+$model = new EntryForm();
+$model->name = "MackZhu";
+$model->email = "Bad Email";
+
+if($model->validate())
+{
+    echo "Success";
+}
+else
+{
+    echo "Error";
+}
 ?>
 <?= Html::encode($message) ?>
